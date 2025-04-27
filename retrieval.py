16c68e0cb4742e0f7bb88363a999c27c4c1ee892
richard-costa <scosta.richard@gmail.com>
2025-04-27 14:21:37 -0300

    add identifier to query

--- retrieval.py
+++ retrieval.py
@@ -2,21 +2,56 @@ import logging
 
 from db_utils import pg_connection
 from embed import embed_query
+from psycopg import sql
+from dataclasses import dataclass
 
+@dataclass
+class Distances:
+    cosine = '<=>'
+    l2 = '<->'
 
-def retrieve_from_pgvector(query: str, db_name: str, table_name: str = 'pg_embeddings', retrieve_k = 5) -> list[str]:
+def retrieve_from_pgvector(query: str, 
+                           db_name: str,
+                           table_name: str = 'pg_embeddings',
+                           with_score: bool = True, 
+                           distance_operator = Distances.cosine,
+                           retrieve_k = 5) -> list[tuple[str, None]] | list[tuple[str, float]]:
+    
     conn = pg_connection(db_name=db_name)
     embedded_query = embed_query(query).tolist()
+    
     search_query = """
         SELECT chunk from {table_name}
-        ORDER BY embedding <-> %s::vector
+        ORDER BY embedding {distance_operator} %s::vector
         LIMIT %s
-        """.format(table_name=table_name)
-    
+        """.format(table_name=table_name, distance_operator=distance_operator)
+
+    if with_score:
+        search_query = """
+                        SELECT 
+                            chunk, 
+                            embedding {distance_operator} %s::vector AS score
+                        FROM 
+                            {table_name}
+                        ORDER BY 
+                            score
+                        LIMIT 
+                            %s;
+                        """.format(table_name=sql.Identifier(table_name), 
+                                   distance_operator=sql.Identifier(distance_operator))
+        
+
+        
     with conn.cursor() as cur:
         cur.execute(search_query, (embedded_query, retrieve_k))
         results = cur.fetchall()
 
     conn.close()
 
-    return [doc[0] for doc in results]
+    return results 
+
+if __name__ == '__main__':
+    res = retrieve_from_pgvector('physics', 'test_db', with_score=True)
+    print(res)
+    print(type(res))
+    print(Distances.cosine)
