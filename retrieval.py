fae6ce8f0b17d753cd26a5cf585657336cbd26db
richard-costa <scosta.richard@gmail.com>
2025-05-01 16:31:00 -0300

    add hybridh search and other adjustments

--- retrieval.py
+++ retrieval.py
@@ -11,17 +11,19 @@ class Distances:
     cosine = '<=>'
     l2 = '<->'
 
-def retrieve_from_pgvector(query: str, 
-                           db_name: str,
-                           table_name: str = 'pg_embeddings_test',
-                           with_score: bool = True, 
-                           distance_operator = Distances.cosine,
-                           retrieve_k = 5) -> list[tuple[str, None]] | list[tuple[str, float]]:
-    
+
+def semantic(
+        query: str, 
+        db_name: str,
+        table_name: str = 'pg_embeddings_test',
+        with_score: bool = True, 
+        distance_operator = Distances.cosine,
+        retrieve_k = 5) -> list[tuple[str, None]] | list[tuple[str, float]]:
+
     conn = pg_connection(db_name=db_name)
     embedded_query = embed_query(query)
     
-    search_query = sql.SQL("""\
+    search_stmt = sql.SQL("""\
         SELECT chunk from {table_name}
         ORDER BY embedding {distance_operator} %s::vector
         LIMIT %s
@@ -29,7 +31,7 @@ def retrieve_from_pgvector(query: str,
                     distance_operator=sql.SQL(distance_operator))
 
     if with_score:
-        search_query = sql.SQL("""\
+        search_stmt = sql.SQL("""\
                         SELECT 
                             chunk, 
                             embedding {distance_operator} %s::vector AS score
@@ -44,9 +46,78 @@ def retrieve_from_pgvector(query: str,
 
         
     with conn.cursor() as cur:
-        cur.execute(search_query, (embedded_query, retrieve_k))
+        cur.execute(search_stmt, (embedded_query, retrieve_k))
         results = cur.fetchall()
 
     conn.close()
 
     return results 
+
+def hybrid(
+        query: str,
+        db_name: str,
+        lang: str = 'english',
+        rrf_k = 60,
+        retrieve_k = 5,
+        table_name: str = 'pg_embeddings_test',
+        distance_operator = Distances.cosine,
+        with_score: bool = True) -> list[tuple[str, None]] | list[tuple[str, float]]:
+    
+    conn = pg_connection(db_name=db_name)
+    embedded_query = embed_query(query)
+
+    search_stmt = sql.SQL("""\
+        SELECT
+            searches.id,
+            searches.chunk,
+            SUM(1.0  / (searches.rank + %s)) AS score -- Inlined rrf_score logic
+        FROM (
+            (
+                SELECT
+                    id,
+                    chunk, 
+                    RANK() OVER (ORDER BY embedding {distance_operator} %s::vector) AS rank
+                FROM {table_name}
+                ORDER BY embedding {distance_operator} %s::vector
+                LIMIT 20
+            )
+            UNION
+            (
+                SELECT
+                    id,
+                    chunk, 
+                    RANK() OVER (ORDER BY ts_rank_cd(to_tsvector(chunk), plainto_tsquery(%s)) DESC) AS rank
+                FROM {table_name}
+                WHERE to_tsvector(%s, chunk) @@ plainto_tsquery(%s, %s)
+                ORDER BY rank
+                LIMIT 20
+            )
+        ) searches
+        GROUP BY searches.id, searches.chunk
+        ORDER BY score DESC
+        LIMIT %s;
+        """).format(table_name=sql.Identifier(table_name), 
+                    distance_operator=sql.SQL(distance_operator))
+        
+    with conn.cursor() as cur:
+        params = (
+            rrf_k,
+            embedded_query,
+            embedded_query,
+            query,
+            lang,
+            lang,
+            query,
+            retrieve_k
+        )
+
+        cur.execute(search_stmt, params)
+        results = cur.fetchall()
+
+    conn.close()
+
+    return results
+
+
+if __name__ == "__main__":
+    print(hybrid('copenhagen', 'test_db'))
